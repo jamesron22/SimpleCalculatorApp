d145d512a2914a5c6f3e6f6bae139fb81cde7dda
Ron <38900227+jamesron22@users.noreply.github.com>
2019-01-18 11:27:55 -0800

    Add files via upload

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                Console.WriteLine("Input first number for calculation");
+                double firstnumber = inputConverter.InputToNum(Console.ReadLine());
+                
+                Console.WriteLine("+, -, *, /");
+                string operation = Console.ReadLine();
+
+                Console.WriteLine("Input second number for calculation");
+                double secondnumber = inputConverter.InputToNum(Console.ReadLine());
+
+                double result = calculatorEngine.Calculate(operation, firstnumber, secondnumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
